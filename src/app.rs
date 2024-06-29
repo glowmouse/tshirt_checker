6393e7aedb85a0863c2cf7d4f0835508836fd673
Andrew Brownbill <andrew.brownbill@gmail.com>
2024-06-29 13:20:24 -0400

    Restore 2nd part image

--- src/app.rs
+++ src/app.rs
@@ -134,6 +134,7 @@ fn blue_to_red(input: &egui::Color32) -> egui::Color32 {
     red_adjust.into()
 }
 
+// Gamma correction tables.
 const GAMMA_17: [u8; 256] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 7,
     7, 7, 8, 8, 9, 9, 10, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 17, 17, 18, 18, 19,
@@ -988,14 +989,13 @@ impl TShirtCheckerApp<'_> {
             "blue_shirt",
             &cc.egui_ctx,
         );
-        //let default_art: LoadedImage = load_image_from_trusted_source(include_bytes!("sf2024-attendee-v1.png"), "default_art", &cc.egui_ctx  );
         let artwork_0: LoadedImage = load_image_from_trusted_source(
             include_bytes!("test_artwork.png"),
             "artwork_0",
             &cc.egui_ctx,
         );
         let artwork_1: LoadedImage = load_image_from_trusted_source(
-            include_bytes!("sf2024-attendee-v2.png"),
+            include_bytes!("sf2024-attendee-v1.png"),
             "artwork_1",
             &cc.egui_ctx,
         );
