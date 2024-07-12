a22b5bbead006fe867a42b00a8ed277c9657b213
Andrew Brownbill <andrew.brownbill@gmail.com>
2024-07-12 13:24:34 -0400

    Restore original sw.js

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
-/*  './index.html',
-  './eframe_template.js', */
-/*  './eframe_template_bg.wasm', */
+  './index.html',
+  './tshirt_cecker.js',
+  './tshirt_checker_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
